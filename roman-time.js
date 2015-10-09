d61bbe6929654a0699ab6f77bcef54bc26e230be
TatarchenkovAndrey <ff962010@mail.ru>
2015-10-09 16:28:53 +0500

    Add solution 2.0

--- roman-time.js
+++ roman-time.js
@@ -1,10 +1,10 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
-var Decada = ['NN', 'X', 'XX', 'XXX', 'XL', 'L'];
-var Units = ['NN', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+var Decada = ['', 'X', 'XX', 'XXX', 'XL', 'L'];  //Я убрал значение нуля как 'NN' из "десятков"
+var Units = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];// и "единиц"
 	hours = Number(hours);
 	minutes = Number(minutes);
-var text1 = 'Ваше время арабскими числами: ';
+var text1 = 'Ваше время арабскими числами(часы и минуты): ';
  
 	if ( (hours > 23) || (hours < 0)) 
 		{
@@ -16,14 +16,21 @@ var text1 = 'Ваше время арабскими числами: ';
 			console.log ('Неверный, к сожалению, ввод');
 			return;
 		}
-
+var checkHours = hours;
+var checkMinutes = minutes;
 console.log (text1 + hours + ':' + minutes);    
 var hoursUnits = hours%10;
 var hoursDecada = (hours - hoursUnits) / 10;                
 var minutesUnits = minutes%10;
 var minutesDecada = (minutes - minutesUnits) / 10;    
 var RomanHoursDecada, RomanhoursUnits, RomanMinutesDecada, RomanMinutesUnits;
-   
+//Добавил проверку, где я проверяю, как введено число. И если там 0, то нулевой элемент массивов меняется
+	if (checkHours === 0 || checkMinutes === 0)
+		{
+			Decada[0] = 'N';
+			Units[0] = 'N';
+		}
+//Дальше все также
 	for (var i = 0; i < Decada.length; i++)
 		{
 			if (hoursDecada === i) RomanHoursDecada = Decada[i];
@@ -40,6 +47,7 @@ var RomanHoursDecada, RomanhoursUnits, RomanMinutesDecada, RomanMinutesUnits;
 		{
 			if (minutesUnits === i) RomanMinutesUnits = Units[i];
 		}
+
 	console.log('Ваше время записывается римскими цифрами как: ');
 	console.log (RomanHoursDecada + RomanHoursUnits + ":" + RomanMinutesDecada + RomanMinutesUnits);
 	console.log('Спасибо, что дождались решения, надеюсь, оно успешно');
